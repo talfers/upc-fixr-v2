59f33b406ac6ff90a9fef0bf6562432c72569077
Taylor Alfers <Taylor>
2019-11-09 09:10:28 -0700

    upload file setup complete

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// APP CONFIG
 // Express app setup
 const express = require('express');
 const app = express();
@@ -6,19 +7,35 @@ const PORT = process.env.PORT || 3000;
 // Setup static dir
 app.use(express.static(__dirname + '/public'));
 
-// BodyParser Setup
-app.use(express.urlencoded({extended: false}));
+// Multer storage config
+const multer = require('multer');
 
+const storage = multer.diskStorage({
+  destination: function(req, file, callback){
+    callback(null, './public/uploads/')
+  },
+  filename: function(req, file, callback){
+    callback(null, 'uploadedFileToCsv.csv')
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// ROUTES
 // Get upload route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/upload.html');
 });
 
 // Post results route
-app.get('/results', (req, res) => {
+app.post('/results', upload.single('file'), (req, res) => {
+  //console.log(req.body.manid);
+  //console.log(req.body.brandid);
   res.sendFile(__dirname + '/views/results.html');
 });
 
+
+// SERVER CONFIG
 // Start express server
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
