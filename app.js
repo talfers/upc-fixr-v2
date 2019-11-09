1c557751ecfb2007cc88fd86d4d9d5ba8b0ba82e
Taylor Alfers <Taylor>
2019-11-09 07:58:06 -0700

    add bodyParser

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+// Express app setup
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Setup static dir
 app.use(express.static(__dirname + '/public'));
 
+// BodyParser Setup
+app.use(express.urlencoded({extended: false}));
+
+// Get upload route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/upload.html');
 });
 
+// Post results route
 app.get('/results', (req, res) => {
   res.sendFile(__dirname + '/views/results.html');
 });
 
+// Start express server
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
