029ccb17afc38f1c40104486198ca8b61d57d5b2
tayloralfers@gmail.com <Taylor>
2019-11-13 20:14:05 -0700

    add start script

--- public/lib/test.py
+++ public/lib/test.py
@@ -4,4 +4,4 @@ import pandas as pd
 df = pd.read_excel('./public/uploads/uploadedFileToCsv.xlsx')
 print(df);
 
-print('Hello form test.py! Variable: ' + sys.argv[1:])
+print('Hello from test.py! Variable: ' + sys.argv[1:])
